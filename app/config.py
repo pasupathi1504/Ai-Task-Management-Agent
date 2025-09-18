34c84948f29ba4f4a4c548721f114762d680cff9
pasupathi1504 <aravindsathya23@gmail.com>
2025-09-18 16:03:48 +0530

    add frontend

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg2://ai_tasks:ai_tasks_pw@localhost:5432/ai_tasks",
-)
